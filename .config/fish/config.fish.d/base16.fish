4473f3e579360053fbb260f0d5ff04e7b9a464d0
Nicholas Hutchinson <nshutchinson@gmail.com>
2014-05-04 18:08:01 +0100

    Update tmux conf

--- .config/fish/config.fish.d/base16.fish
+++ .config/fish/config.fish.d/base16.fish
@@ -1,5 +1,7 @@
 set -l BASE16_SCHEME ocean
 set -l BASE16_SHELL "$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
-if test -f $BASE16_SHELL
+
+if test -f "$BASE16_SHELL" -a -z "$BASE16_SCHEME_CURRENT"
   eval sh "$BASE16_SHELL"
+  set -x BASE16_SCHEME_CURRENT "$BASE16_SCHEME"
 end

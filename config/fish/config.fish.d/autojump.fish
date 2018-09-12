4af56255c462516febfa034d4486e5905a7d5c7e
Nick Hutchinson <nick.hutchinson@filigree.tech>
2018-09-12 11:55:00 +0100

    Update dotfiles

--- config/fish/config.fish.d/autojump.fish
+++ config/fish/config.fish.d/autojump.fish
@@ -1,6 +1,9 @@
 if test -f "$HOME/.autojump/etc/profile.d/autojump.fish"
   . "$HOME/.autojump/etc/profile.d/autojump.fish"
 
+else if test -f /usr/local/share/autojump/autojump.fish
+  . /usr/local/share/autojump/autojump.fish
+
 else if test -f /etc/profile.d/autojump.fish
   . /etc/profile.d/autojump.fish
 end
